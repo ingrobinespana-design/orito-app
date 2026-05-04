a6eb0c59cfb6477f3398a6be1c44f81714778efc
darwinespaña <ingrobinespna@gmail.com>
2026-05-04 14:28:10 -0500

    migrar a postgresql

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./orito.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./orito.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
